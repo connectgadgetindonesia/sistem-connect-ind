df60447c9ec91cc4fb2a5a9c0be4cd563687f044
CONNECT IND <connectind@Mac-mini-CONNECT.local>
2025-07-23 09:58:10 +0700

    Fix: tambah import Link di riwayat.js

--- pages/riwayat.js
+++ pages/riwayat.js
@@ -1,5 +1,6 @@
 import Layout from '@/components/Layout'
 import { useEffect, useState } from 'react'
+import Link from 'next/link';
 import { supabase } from '../lib/supabaseClient'
 import * as XLSX from 'xlsx'
 
