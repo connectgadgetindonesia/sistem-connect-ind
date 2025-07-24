3fef96ac83a2925873104e48f7422233d93be385
CONNECT IND <connectind@Mac-mini-CONNECT.local>
2025-07-24 15:12:26 +0700

    Final minimalist invoice layout tanpa background, clean & responsive

--- components/InvoicePDF.jsx
+++ components/InvoicePDF.jsx
@@ -58,25 +58,23 @@ export default function InvoicePDF() {
           padding: "32px",
           borderRadius: "20px",
           position: "relative",
-          boxSizing: "border-box"
+          boxSizing: "border-box",
+          color: "#000"
         }}
       >
-        {/* Logo + INVOICE */}
-        <div style={{ textAlign: "center", marginBottom: "20px" }}>
-          <img src="/logo-connect-transparan.png" alt="Logo" width="28" />
-          <h1 style={{ margin: "8px 0 0", fontSize: "18px" }}>INVOICE</h1>
+        {/* Logo + INVOICE Center */}
+        <div style={{ textAlign: "center", marginBottom: "16px" }}>
+          <img src="/logo-connect-transparan.png" alt="Logo" width="28" style={{ display: "inline-block", verticalAlign: "middle" }} />
+          <h1 style={{ display: "inline-block", margin: "0 0 0 8px", fontSize: "18px" }}>INVOICE</h1>
         </div>
 
-        {/* 3-Column Info */}
+        {/* Tiga Kolom Info */}
         <div style={{ display: "flex", justifyContent: "space-between", fontSize: "10px", marginBottom: "24px" }}>
-          {/* Invoice Detail */}
           <div style={{ width: "33%" }}>
             <strong>Invoice Details</strong><br />
             Invoice number: {data.invoice_id}<br />
             Invoice date: {data.tanggal}
           </div>
-
-          {/* Toko */}
           <div style={{ width: "33%", textAlign: "center" }}>
             <strong>CONNECT.IND</strong><br />
             (+62) 896-31-4000-31<br />
@@ -84,8 +82,6 @@ export default function InvoicePDF() {
             Kalibanteng Kulon, Semarang Barat<br />
             Kota Semarang, Jawa Tengah 50145
           </div>
-
-          {/* Customer */}
           <div style={{ width: "33%", textAlign: "right" }}>
             <strong>Invoice To:</strong><br />
             {data.nama_pembeli}<br />
@@ -94,15 +90,13 @@ export default function InvoicePDF() {
           </div>
         </div>
 
-        {/* Produk Box */}
+        {/* Box Produk */}
         <div style={{
-          width: "100%",
           border: "1px solid #000",
           borderRadius: "10px",
           padding: "12px",
           fontSize: "11px",
-          marginBottom: "16px",
-          boxSizing: "border-box"
+          marginBottom: "180px"
         }}>
           <div style={{ fontWeight: "600", marginBottom: 4 }}>{data.nama_produk}</div>
           <div>SN / SKU: {data.sn_sku}</div>
@@ -116,14 +110,8 @@ export default function InvoicePDF() {
           </div>
         </div>
 
-        {/* Total Summary */}
-        <div style={{
-          width: "100%",
-          fontSize: "11px",
-          display: "flex",
-          justifyContent: "space-between",
-          marginTop: "220px"
-        }}>
+        {/* Notes dan Total */}
+        <div style={{ display: "flex", justifyContent: "space-between", fontSize: "10px" }}>
           <div style={{ color: "#888" }}>
             <strong>Notes:</strong><br />
             Terima kasih telah berbelanja di CONNECT.IND.<br />
