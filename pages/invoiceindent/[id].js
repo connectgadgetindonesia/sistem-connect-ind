621f0518a983688b259597c8d39a582d3e7d434c
CONNECT IND <connectind@Mac-mini-CONNECT.local>
2025-07-25 11:08:25 +0700

    Update fitur

--- pages/invoiceindent/[id].js
+++ pages/invoiceindent/[id].js
@@ -1,12 +1,16 @@
 import dynamic from "next/dynamic";
 
+// Import komponen InvoiceIndent dengan SSR dimatikan
 const InvoiceIndent = dynamic(() => import("../../components/InvoiceIndent"), {
   ssr: false,
 });
 
 export async function getServerSideProps(context) {
   const { id } = context.query;
-  return { props: { id } };
+
+  return {
+    props: { id }, // Kirim id ke komponen
+  };
 }
 
 export default function InvoiceIndentPage({ id }) {
